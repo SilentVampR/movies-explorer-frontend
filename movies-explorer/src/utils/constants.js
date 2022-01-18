3addcf5c8610211d64e06f501abbc3d8af17f9d2
Корсунов Евгений Владимирович <e.korsunov@2gis.ru>
2022-01-18 09:28:54 +0900

    installed RCA add some folders etc

---
+++ movies-explorer/src/utils/constants.js
@@ -0,0 +1,19 @@
+export const apiToken = "beb35b1a-7d6a-4743-95a3-06e29b783755";
+export const yandexMestoApiURL = "https://mesto.nomoreparties.co/v1/" + "cohort-27";
+export const myId = 'dab90f1d297ca4e046afdd19';
+
+export const classNamesSettings = {
+  formSelector: '.popup__form-container',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__submit-button',
+  inactiveButtonClass: 'popup__submit-button_disabled',
+  inputErrorClass: 'popup__input_state_not-valid',
+  errorClass: 'popup__text-error_state_not-valid',
+  inputContainer: '.popup__input-container',
+  errorText: '.popup__text-error',
+  templateId: '#cardTemplate',
+  profileNameSelector: '.profile__name',
+  profileAboutSelector: '.profile__about',
+  profileAvatarSelector: '.profile__avatar',
+  sectionCards: '.cards',
+}
